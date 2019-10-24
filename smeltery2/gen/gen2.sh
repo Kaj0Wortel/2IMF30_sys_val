e08044de5e7b272a5dd38a4fd35d547f4ec3f406
Kaj0Wortel <kaj.wortel@gmail.com>
2019-10-24 13:09:23 +0200

    Added counter example feature

--- smeltery2/gen/gen2.sh
+++ smeltery2/gen/gen2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo -e "\nGENERATING $1"
 set -x
 lps2pbes -v --formula="../properties/$1.mcf" "smeltery2.lps" "smeltery2_$1.pbes"
-pbessolve -v --strategy=1 --rewriter='jittyc' --search='depth-first' "smeltery2_$1.pbes"
+pbessolve -v --strategy=1 --rewriter='jittyc' --search='depth-first' --evidence-file= "smeltery2_$1.pbes"
