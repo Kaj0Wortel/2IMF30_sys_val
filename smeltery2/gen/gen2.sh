899d7ba358b70db4634b3a36530be0f67c421b65
Kaj0Wortel <kaj.wortel@gmail.com>
2019-10-24 13:56:20 +0200

    added evidence

--- smeltery2/gen/gen2.sh
+++ smeltery2/gen/gen2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
 lps2pbes -v --formula="../properties/$1.mcf" "smeltery2.lps" "smeltery2_$1.pbes"
-pbessolve -v --strategy=1 --rewriter='jittyc' --search='depth-first' --evidence-file= "smeltery2_$1.pbes"
+pbessolve -v --strategy=1 --rewriter='jittyc' --search='depth-first' --file="smeltery2.lps" "smeltery2_$1.pbes" --evidence-file="evidence/smeltery2_$1.evidence.lps"
+rm -f "smeltery2_$1.pbes"
--- smeltery2/gen/run2.sh
+++ smeltery2/gen/run2.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 mcrl22lps -v "../smeltery2_spec.mcrl2" "smeltery2.lps"
 
+rm -fR "out"
+rm -fR "evidence"
+mkdir "evidence"
+
 (for f in "$@"; do
     echo "$f"
-done) | parallel --will-cite -j $(nproc) --results "log_{}/" --timeout 3600 --progress "./gen2.sh"
+done) | parallel --will-cite -j $(nproc) --results "out/log_{}/" --timeout 3600 --progress "./gen2.sh"
+
+output_file="result.txt"
+rm -f $output_file
+for f in "$@"; do
+    if "out/log_$f/1/$f/stdout"; then
+        echo "$f"": true" >> $output_file
+    else
+        echo "$f"": false" >> $output_file
+        lps2lts "evidence/smeltery2_$f.evidence.lps" "evidence/smeltery2_$f.evidence.lts"
+        ltsgraph "evidence/smeltery2_$f.evidence.lts"
+    fi
+    
+    rm -f "evidence/smeltery2_$f.evidence.lps"
+done
+
+rm -f "smeltery2.lps"
