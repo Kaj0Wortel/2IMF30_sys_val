db272be9ddd96bc3d2be78ea8aef838a7685e2d1
Kaj0Wortel <kaj.wortel@gmail.com>
2019-10-26 21:13:58 +0200

    Updated scripts + R12 & R15 & R17

--- smeltery2/gen/gen2.sh
+++ smeltery2/gen/gen2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 lps2pbes -v --formula="../properties/$1.mcf" --counter-example "smeltery2.lps" "smeltery2_$1.pbes"
-pbessolve -v --strategy=1 --rewriter='jittyc' --evidence-file="evidence/smeltery2_$1.evidence.lps" --search='depth-first' --file="smeltery2.lps" "smeltery2_$1.pbes" 
+pbessolve -v --strategy=3 --rewriter='jittyc' --evidence-file="evidence/smeltery2_$1.evidence.lps" --search='depth-first' --file="smeltery2.lps" "smeltery2_$1.pbes"
 rm -f "smeltery2_$1.pbes"
