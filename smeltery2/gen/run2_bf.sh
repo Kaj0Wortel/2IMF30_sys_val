a55c5a50d7897bdb382e744cd568ab4c5647b1fc
Kaj0Wortel <kaj.wortel@gmail.com>
2019-10-26 18:18:29 +0200

    Added breadth-first search server scripts

---
+++ smeltery2/gen/run2_bf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo chmod -R 777 .
+mcrl22lps -v "../smeltery2_spec.mcrl2" "smeltery2.lps"
+
+rm -fR "out"
+rm -fR "evidence"
+mkdir "evidence"
+
+(for f in "$@"; do
+    echo "$f"
+done) | parallel --will-cite -j $(nproc) --results "out/log_{}/" --timeout 3600 --progress "./gen2_bf.sh"
+
+sudo chmod -R 777 .
+sudo ./process_results.sh
